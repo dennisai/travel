c175653e739e75936ebff3d131f977b1e741ca79
Dennis Ai <dennis.q.ai@gmail.com>
2012-05-09 20:09:20 -0500

    Instantiated relevance model

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Relevance(models.Model):
+    concept = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    relevance = models.IntegerField()
+    explanation = models.TextField()
